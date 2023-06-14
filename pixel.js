5d87a28e6ec5f93a9c5f2c4e243ea449413aa024
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-06-14 20:27:40 +0200

    pixelniarshop.js

--- pixel.js
+++ pixel.js
@@ -1,4 +1,16 @@
-        !function(f,b,e,v,n,t,s)
+ // CLARITY
+
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "hjvrrxdnfv");
+
+
+// FACEBOOK PIXEL
+
+
+  !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -6,16 +18,5 @@
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '610180697316002');
-            fbq('track', 'PageView');
-
-            fbq('init', '969029744289057');
-            fbq('track', 'PageView');
-     
-            fbq('init', '263270092690437');
-            fbq('track', 'PageView');
-
-            fbq('init', '637792418174114');
-            fbq('track', 'PageView');
-    
-    
+  fbq('init', '147565934999182');
+  fbq('track', 'PageView');
